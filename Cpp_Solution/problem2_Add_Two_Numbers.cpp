b6fc409badabd669b4b6a2a073677af2a92a7f2a
xiaowang1105 <wx961105@outlook.com>
2018-04-07 22:23:47 +0800

    problem2

--- Cpp_Solution/problem2_Add_Two_Numbers.cpp
+++ Cpp_Solution/problem2_Add_Two_Numbers.cpp
@@ -8,3 +8,33 @@
 #include<iostream>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+    public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode head{0};
+        auto curr = &head;
+        auto carry = 0;
+        while(l1 || l2 || carry){
+            auto a = l1 ? l1 -> val : 0, b = l2 ? l2 -> val : 0;
+            auto val = a + b + carry;
+            curr -> next = new ListNode{val % 10};
+            carry = val / 10;
+            l1 = l1 ? l1 -> next : nullptr;
+            l2 = l2 ? l2 -> next : nullptr;
+            curr = curr -> next;
+        }
+        return head.next;
+    }
+
+};
+
+int main(){
+
+    return 0;
+}
