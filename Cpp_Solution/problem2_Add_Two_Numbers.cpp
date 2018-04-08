c8358e33be46f9d5484cb79797251703cacdccec
xiaowang1105 <wx961105@outlook.com>
2018-04-08 11:44:24 +0800

    test part for problem2

--- Cpp_Solution/problem2_Add_Two_Numbers.cpp
+++ Cpp_Solution/problem2_Add_Two_Numbers.cpp
@@ -23,7 +23,7 @@ class Solution {
         while(l1 || l2 || carry){
             auto a = l1 ? l1 -> val : 0, b = l2 ? l2 -> val : 0;
             auto val = a + b + carry;
-            curr -> next = new ListNode{val % 10};
+            curr -> next = new ListNode(val % 10);
             carry = val / 10;
             l1 = l1 ? l1 -> next : nullptr;
             l2 = l2 ? l2 -> next : nullptr;
@@ -35,6 +35,26 @@ class Solution {
 };
 
 int main(){
+    ListNode l1{0};
+    auto curr = &l1;
+    curr -> next = new ListNode(2), curr = curr -> next;
+    curr -> next = new ListNode(4), curr = curr -> next;
+    curr -> next = new ListNode(3), curr = curr -> next;
+    ListNode l2{0};
+    curr = &l2;
+    curr -> next = new ListNode(5), curr = curr -> next;
+    curr -> next = new ListNode(6), curr = curr -> next;
+    curr -> next = new ListNode(4), curr = curr -> next;
 
+    Solution solu;
+    curr = solu.addTwoNumbers(l1.next, l2.next);
+    cout << "input number 1 is: " << l1.next -> val << "->"  
+        << l1.next -> next -> val << "->" << l1.next -> next -> next -> val << endl;
+
+    cout << "input number 2 is: " << l2.next -> val << "->"  
+        << l2.next -> next -> val << "->" << l2.next -> next -> next -> val << endl;
+    cout << "output number is: " << curr -> val << "->"  
+        << curr -> next -> val << "->" << curr -> next -> next -> val << endl;
+    
     return 0;
 }
